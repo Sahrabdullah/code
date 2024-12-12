b5f8a07584008b660d50b5471c10f3f7ba0cfb81
sahrabullah7 <145543132+sahrabullah7@users.noreply.github.com>
2024-12-13 00:34:15 +0200

    users
    users table

--- db.js
+++ db.js
@@ -1,3 +1,13 @@
 const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database('car_search.db')
 
+//USERS TABLE
+const create_users_table = `CREATE TABLE IF NOT EXISTS USERS
+(ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+NAME TEXT NOT NULL, 
+EMAIL TEXT UNIQUE NOT NULL, 
+PASSWORD TEXT NOT NULL, 
+USER_ROLE TEXT NOT NULL, 
+ISADMIN INT)`
+
+
