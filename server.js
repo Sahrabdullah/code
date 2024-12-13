34c8eee3a16859b7f192949097360986508e194a
sahrabullah7 <145543132+sahrabullah7@users.noreply.github.com>
2024-12-13 03:41:01 +0200

    register
    add user

--- server.js
+++ server.js
@@ -5,3 +5,22 @@ const db = db_access.db
 const port = 127
 server.use(express.json()) //instead of using body parser
 
+//USER
+//REGISTERATION
+server.post(`/user/register`, (req, res) => { //req body
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+    const user_role = req.body.user_role
+    const content = `INSERT INTO USERS (NAME,EMAIL,PASSWORD,USER_ROLE, ISADMIN) 
+                VALUES ('${name}', '${email}','${password}', '${user_role}',0)`
+    db.run(content, (err) => {
+        if (err) {
+            console.log(err.message)
+            return res.status(401).send(`Registration Failed`)     //401 is unauthorized user
+        }
+        else
+            return res.status(200).send(`Registered Successfully`)      //20 is OK
+    })
+})
+
